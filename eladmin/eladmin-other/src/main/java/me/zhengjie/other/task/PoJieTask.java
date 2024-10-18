997ec213ec0eec8cd5554521c573c50aede7ff9e
genghui <chunhui128@outlook.com>
2024-10-19 00:39:01 +0800

    51破解自动签到

--- eladmin/eladmin-other/src/main/java/me/zhengjie/other/task/PoJieTask.java
+++ eladmin/eladmin-other/src/main/java/me/zhengjie/other/task/PoJieTask.java
@@ -1,21 +1,23 @@
 package me.zhengjie.other.task;
 
 
-import com.alibaba.fastjson.JSON;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.RandomUtil;
+import cn.hutool.json.JSONUtil;
+import com.microsoft.playwright.*;
+import com.microsoft.playwright.options.ColorScheme;
+import com.microsoft.playwright.options.Cookie;
 import lombok.extern.slf4j.Slf4j;
 import me.zhengjie.utils.RedisUtils;
+import me.zhengjie.utils.enums.RedisKeyEnum;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
-import org.htmlunit.*;
-import org.htmlunit.html.HtmlAnchor;
-import org.htmlunit.html.HtmlImage;
-import org.htmlunit.html.HtmlPage;
-import org.htmlunit.util.Cookie;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.net.URL;
+import java.util.Date;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @Service("poJieTask")
@@ -23,7 +25,6 @@ public class PoJieTask {
 
     @Resource
     private RedisUtils redisUtils;
-    // https://blog.csdn.net/paq6411/article/details/140814778
 
     public static void main(String[] args) throws Exception {
         String cookieStr = "";
@@ -35,53 +36,57 @@ public class PoJieTask {
             throw new RuntimeException("cookie不能为空");
         }
 
-//        RedisKeyEnum keyEnum = RedisKeyEnum.CHECK_IN_52_PO_JIE_TASK;
-//        boolean lock = redisUtils.setIfAbsent(keyEnum.getKey(), keyEnum.getDesc(), 10, TimeUnit.MINUTES);
-//        log.info("定时任务：{}，获取锁：{}", keyEnum.getDesc(), lock);
-//        if (!lock) return;
+        RedisKeyEnum keyEnum = RedisKeyEnum.CHECK_IN_52_PO_JIE_TASK;
+        boolean lock = redisUtils.setIfAbsent(keyEnum.getKey(), keyEnum.getDesc(), 10, TimeUnit.MINUTES);
+        log.info("定时任务：{}，获取锁：{}", keyEnum.getDesc(), lock);
+        if (!lock) return;
 
         // 随机暂停一会
-//        TimeUnit.MINUTES.sleep(RandomUtil.randomInt(1, 8));
+        TimeUnit.MINUTES.sleep(RandomUtil.randomInt(1, 8));
 
-        try (WebClient webClient = new WebClient(BrowserVersion.CHROME)) {
-            webClient.getOptions().setCssEnabled(true);//关闭css
-            webClient.getOptions().setJavaScriptEnabled(true);//开启js
-            webClient.getOptions().setRedirectEnabled(true);//重定向
-            webClient.getOptions().setThrowExceptionOnScriptError(true);//关闭js报错
-            webClient.getOptions().setTimeout(50000);//超时时间
-            webClient.getCookieManager().setCookiesEnabled(true);//允许cookie
-            webClient.setAjaxController(new NicelyResynchronizingAjaxController());//设置支持AJAX
-            webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);//关闭404报错
+        try (Playwright playwright = Playwright.create()) {
+            BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
+//            launchOptions.setHeadless(false); //取消无头模式，我们才能看见浏览器操作
+            launchOptions.setSlowMo(100); //减慢执行速度，以免太快
+//            launchOptions.setDevtools(true); //打开浏览器开发者工具，默认不打开
+            Browser browser = playwright.chromium().launch(launchOptions);
 
-            // 添加cookie
-            List<Cookie> cookieList = JSON.parseArray(cookieStr, Cookie.class);
+            Browser.NewContextOptions newContextOptions = new Browser.NewContextOptions();
+            newContextOptions.setColorScheme(ColorScheme.DARK); //设置浏览器主题，chromium设置了dark好像没用
+            newContextOptions.setViewportSize(1000, 500); //设置浏览器打开后窗口大小
+            BrowserContext browserContext = browser.newContext(newContextOptions);
+
+            // 设置cookie
+            List<Cookie> cookieList = JSONUtil.toList(cookieStr, Cookie.class);
             if (CollectionUtils.isEmpty(cookieList)) {
                 throw new RuntimeException("cookie不能为空");
             }
-            for (Cookie cookie : cookieList) {
-                webClient.getCookieManager().addCookie(cookie);
-            }
+            browserContext.addCookies(cookieList);
 
-            // 发送请求
-            WebRequest webRequest = new WebRequest(new URL("https://www.52pojie.cn"));
-            HtmlPage homePage = webClient.getPage(webRequest);
+
+            Page page = browserContext.newPage();
+            // 添加初始化脚本来隐藏 webdriver 属性
+            String js = "Object.defineProperty(navigator, 'webdriver', {get: () => false});";
+            page.addInitScript(js);
+            page.navigate("https://www.52pojie.cn");
+
             // 判断有没有签到元素，有的话进行签到
-            HtmlAnchor cheackIn = homePage.getFirstByXPath(String.format("//a[img[@src='%s']]", "https://static.52pojie.cn/static/image/common/qds.png"));
-            if (cheackIn != null) {
-                webClient.waitForBackgroundJavaScript(10 * 1000); // 等待 JavaScript 执行完成
-                HtmlPage clickPage = cheackIn.click();
-                webClient.waitForBackgroundJavaScript(10 * 1000); // 等待 JavaScript 执行完成
-                String redirectedUrl = clickPage.getUrl().toString();
-                System.err.println(redirectedUrl);
+            Locator checkInBtn = page.locator(String.format("//a[img[@src='%s']]", "https://static.52pojie.cn/static/image/common/qds.png"));
+            if (checkInBtn.count() > 0) {
+                checkInBtn.first().click();
+                // 睡眠一分钟，等待签到
+                TimeUnit.MINUTES.sleep(1L);
                 return;
             }
-            // 判断有没有已经签到的元素，没有加抛出异常
-            HtmlImage hasCheck = homePage.getFirstByXPath(String.format("//p/img[@src='%s']", "https://static.52pojie.cn/static/image/common/wbs.png"));
-            if (hasCheck == null) {
+            // 判断有没有已经签到的元素，18点之后还没有签到成功，抛出异常
+            Locator hasCheckInBtn = page.locator(String.format("//p/img[@src='%s']", "https://static.52pojie.cn/static/image/common/wbs.png"));
+            if (hasCheckInBtn.count() == 0 && DateUtil.hour(new Date(), true) > 18) {
                 throw new RuntimeException("签到失败");
             }
+
+
         } finally {
-//            redisUtils.del(keyEnum.getKey());
+            redisUtils.del(keyEnum.getKey());
         }
 
     }
