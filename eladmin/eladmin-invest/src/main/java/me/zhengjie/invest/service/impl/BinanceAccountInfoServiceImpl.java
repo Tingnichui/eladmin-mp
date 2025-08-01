0789fbcda75659c81f3ab5ab2712e857785bb2d0
genghui <chunhui128@outlook.com>
2025-08-02 00:43:34 +0800

    优化

--- eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/service/impl/BinanceAccountInfoServiceImpl.java
+++ eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/service/impl/BinanceAccountInfoServiceImpl.java
@@ -15,24 +15,25 @@
 */
 package me.zhengjie.invest.service.impl;
 
+import me.zhengjie.exception.BadRequestException;
 import me.zhengjie.invest.domain.BinanceAccountInfo;
-import me.zhengjie.utils.FileUtil;
+import me.zhengjie.utils.*;
 import lombok.RequiredArgsConstructor;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import me.zhengjie.invest.service.BinanceAccountInfoService;
 import me.zhengjie.invest.domain.vo.BinanceAccountInfoQueryCriteria;
 import me.zhengjie.invest.mapper.BinanceAccountInfoMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import me.zhengjie.utils.PageUtil;
+
 import java.util.List;
 import java.util.Map;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
-import me.zhengjie.utils.PageResult;
 
 /**
 * @description 服务实现
@@ -44,6 +45,13 @@ import me.zhengjie.utils.PageResult;
 public class BinanceAccountInfoServiceImpl extends ServiceImpl<BinanceAccountInfoMapper, BinanceAccountInfo> implements BinanceAccountInfoService {
 
     private final BinanceAccountInfoMapper binanceAccountInfoMapper;
+
+    @Value("${data.rsa.public_key}")
+    private String rsaPublicKey;
+
+    @Value("${data.rsa.private_key}")
+    private String rsaPrivateKey;
+
 
     @Override
     public PageResult<BinanceAccountInfo> queryAll(BinanceAccountInfoQueryCriteria criteria, Page<Object> page){
@@ -58,7 +66,36 @@ public class BinanceAccountInfoServiceImpl extends ServiceImpl<BinanceAccountInf
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void create(BinanceAccountInfo resources) {
+        this.encryptAccountInfo(resources);
         save(resources);
+    }
+
+    private void encryptAccountInfo(BinanceAccountInfo resources) {
+        try {
+            if (StringUtils.isNotBlank(resources.getApiKey())) {
+                resources.setApiKey(RsaUtils.encryptByPublicKey(rsaPublicKey, resources.getApiKey()));
+            }
+            if (StringUtils.isNotBlank(resources.getApiSecret())) {
+                resources.setApiSecret(RsaUtils.encryptByPublicKey(rsaPublicKey, resources.getApiSecret()));
+            }
+        } catch (Exception e) {
+            log.error("币安账户加密失败", e);
+            throw new BadRequestException("加密失败");
+        }
+    }
+
+    private void decryptAccountInfo(BinanceAccountInfo resources) {
+        try {
+            if (StringUtils.isNotBlank(resources.getApiKey())) {
+                resources.setApiKey(RsaUtils.decryptByPrivateKey(rsaPrivateKey, resources.getApiKey()));
+            }
+            if (StringUtils.isNotBlank(resources.getApiSecret())) {
+                resources.setApiSecret(RsaUtils.decryptByPrivateKey(rsaPrivateKey, resources.getApiSecret()));
+            }
+        } catch (Exception e) {
+            log.error("币安账户加密失败", e);
+            throw new BadRequestException("加密失败");
+        }
     }
 
     @Override
@@ -66,6 +103,7 @@ public class BinanceAccountInfoServiceImpl extends ServiceImpl<BinanceAccountInf
     public void update(BinanceAccountInfo resources) {
         BinanceAccountInfo binanceAccountInfo = getById(resources.getId());
         binanceAccountInfo.copy(resources);
+        this.encryptAccountInfo(binanceAccountInfo);
         saveOrUpdate(binanceAccountInfo);
     }
 
