589ee7f0782d36f52c1b91d442b84fbf27dcba24
genghui <chunhui128@outlook.com>
2025-10-17 16:03:45 +0800

    修改锁仓逻辑

--- eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/rest/BinanceTradeInfoController.java
+++ eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/rest/BinanceTradeInfoController.java
@@ -115,7 +115,7 @@ public class BinanceTradeInfoController {
     @ApiOperation("同步交易")
     @PreAuthorize("@el.check('binanceTradeInfo:sync')")
     public ResponseEntity<BinanceTradeStatsInfoVO> syncHedge(){
-        binanceTradeInfoService.syncHedge();
+        binanceFuturesTradeInfoService.stats();
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
