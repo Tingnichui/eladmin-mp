e75a2e689f7ea1c1de7a0f2adcdbe17a01dcbf57
genghui <chunhui128@outlook.com>
2025-10-31 10:10:17 +0800

    1

--- eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/util/TradeMatcherUtil.java
+++ eladmin/eladmin-invest/src/main/java/me/zhengjie/invest/util/TradeMatcherUtil.java
@@ -43,47 +43,56 @@ public class TradeMatcherUtil {
         while (closeIt.hasNext()) {
             T close = closeIt.next();
 
-            Iterator<T> openIt = openList.iterator();
-            while (openIt.hasNext()) {
-                T open = openIt.next();
+            for (int i = 0; i < 2; i++) {
 
-                // 跳过数量为 0 的开仓单
-                if (qtyGetter.apply(open).compareTo(BigDecimal.ZERO) <= 0) {
-                    openIt.remove();
-                    continue;
-                }
-
-                // 忽略 平仓时间早于开仓时间的
-                if (timeGetter.apply(close).before(timeGetter.apply(open))) {
-                    continue;
-                }
-
-                // 计算撮合数量
-                BigDecimal matchQty = qtyGetter.apply(close).min(qtyGetter.apply(open));
-
-                // 创建撮合记录
-                MatchedTradeInfo matched = new MatchedTradeInfo(side, feeRate);
-                matched.setQty(matchQty);
-                matched.setOpenPrice(priceGetter.apply(open));
-                matched.setClosePrice(priceGetter.apply(close));
-
-                if (!filter.test(matched)) {
-                    continue;
-                }
-
-                // 假如撮合列表
-                matchedList.add(matched);
-
-                // 更新剩余数量
-                qtySetter.accept(open, qtyGetter.apply(open).subtract(matchQty));
-                qtySetter.accept(close, qtyGetter.apply(close).subtract(matchQty));
-
-                // 平仓单撮合完则移除
                 if (qtyGetter.apply(close).compareTo(BigDecimal.ZERO) <= 0) {
-                    closeIt.remove();
                     break;
                 }
+
+                Iterator<T> openIt = openList.iterator();
+                while (openIt.hasNext()) {
+                    T open = openIt.next();
+
+                    // 跳过数量为 0 的开仓单
+                    if (qtyGetter.apply(open).compareTo(BigDecimal.ZERO) <= 0) {
+                        openIt.remove();
+                        continue;
+                    }
+
+                    // 忽略 平仓时间早于开仓时间的
+                    if (i == 0 && timeGetter.apply(close).before(timeGetter.apply(open))) {
+                        continue;
+                    }
+
+                    // 计算撮合数量
+                    BigDecimal matchQty = qtyGetter.apply(close).min(qtyGetter.apply(open));
+
+                    // 创建撮合记录
+                    MatchedTradeInfo matched = new MatchedTradeInfo(side, feeRate);
+                    matched.setQty(matchQty);
+                    matched.setOpenPrice(priceGetter.apply(open));
+                    matched.setClosePrice(priceGetter.apply(close));
+
+                    if (i == 0 && !filter.test(matched)) {
+                        continue;
+                    }
+
+                    // 假如撮合列表
+                    matchedList.add(matched);
+
+                    // 更新剩余数量
+                    qtySetter.accept(open, qtyGetter.apply(open).subtract(matchQty));
+                    qtySetter.accept(close, qtyGetter.apply(close).subtract(matchQty));
+
+                    // 平仓单撮合完则移除
+                    if (qtyGetter.apply(close).compareTo(BigDecimal.ZERO) <= 0) {
+                        closeIt.remove();
+                        break;
+                    }
+                }
+
             }
+
         }
 
         return matchedList;
