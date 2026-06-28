0a2cee90488d6f9a67435cef7bbf280b58528b49
genghui <chunhui128@outlook.com>
2026-06-28 11:29:13 +0800

    K线周期

--- eladmin/eladmin-system/src/test/java/me/zhengjie/UtilsTests.java
+++ eladmin/eladmin-system/src/test/java/me/zhengjie/UtilsTests.java
@@ -71,7 +71,7 @@ public class UtilsTests {
         String params = "[\n" +
                 "    {\n" +
                 "        \"symbol\": \"BTCUSDT\",\n" +
-                "        \"intervalCodes\": \"5m,15m,1h,2h,4h,12h,1d,1w,1M\",\n" +
+                "        \"intervalCodes\": \"5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M\",\n" +
                 "        \"defaultStartTime\": \"2017-08-16\"\n" +
                 "    }\n" +
                 "]";
